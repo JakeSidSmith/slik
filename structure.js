fffb8cacc9c938da71555bb2aab446753f02bb3a
JakeSidSmith <jake@dabapps.com>
2014-08-07 09:07:04 +0100

    More jquery-like syntax

--- structure.js
+++ structure.js
@@ -68,3 +68,51 @@ person.flo.do(
     },
   }
 );
+
+/* Possible jquery-like syntax */
+/*-----------------------------*/
+
+var position1 = {
+  upperBody: {
+    body: 0,
+    head: 0,
+    arms: 0,
+  },
+  lowerBody: {
+    legs: 0
+  }
+}
+
+// Initialise flo object
+var myFlo = flo(position1);
+
+// Set new position object to modify
+myFlo.position(position2);
+
+// Set animation
+myFlo.animation('animation')
+  // Set animation attributes
+  .duration(500);
+
+// Get animation attributes
+var animationDuration = myFlo.animation('animation').duration();
+
+// Clear animation attributes
+myFlo.animation('animation').clear('duration');
+
+// Clear animation
+myFlo.animation('animation').clear();
+
+// Do animation
+myFlo.do('animation');
+
+// Do animation with new attributes
+myFlo.do('animation')
+  // Set new attributes
+  .duration(200);
+
+// Change attributes during animation
+myFlo.duration(1000);
+
+// Get current animation attributes
+myFlo.duration();
