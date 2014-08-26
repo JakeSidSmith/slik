c54274500071a45c585ce9929d23c651ac8404e9
JakeSidSmith <jake@dabapps.com>
2014-08-26 22:35:55 +0100

    Wrap everything in function & fix jshint warnings

--- flo.js
+++ flo.js
@@ -1,176 +1,182 @@
-'user strict';
+'use strict';
 
-var flo = function (position) {
+(function () {
 
-  var floObject = {
-    position: position,
-    animations: {},
-    requestAnimationFrame: function (func) {
-      var requestAnimationFrame = window.requestAnimationFrame ||
-      window.mozRequestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
-      window.msRequestAnimationFrame;
-      requestAnimationFrame(func);
-    },
-    isFullscreen: function () {
-      return document.fullscreenElement ||
-        document.msFullscreenElement ||
-        document.mozFullScreenElement ||
-        document.webkitFullscreenElement;
-    },
-    enterFullscreen: function (element) {
-      if (element.requestFullscreen) {
-        element.requestFullscreen();
-      } else if (element.msRequestFullscreen) {
-        element.msRequestFullscreen();
-      } else if (element.mozRequestFullScreen) {
-        element.mozRequestFullScreen();
-      } else if (element.webkitRequestFullscreen) {
-        element.webkitRequestFullscreen();
-      }
-    },
-    exitFullscreen: function () {
-      if (document.exitFullscreen) {
-        document.exitFullscreen();
-      } else if (document.msExitFullscreen) {
-        document.msExitFullscreen();
-      } else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-      } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-      }
-    },
-    onDocumentReady: function (func) {
-      var domContentLoaded = false;
-      var runFunction = function () {
-        if (!domContentLoaded) {
-          domContentLoaded = true;
-          func();
-        }
-      };
-      if (document.addEventListener) {
-        document.addEventListener('DOMContentLoaded', runFunction, false);
-      }
-      if (document.attachEvent) {
-        document.attachEvent('onreadystatechange', runFunction);
-      }
-      document.onload = runFunction;
-    }
-  };
+  var flo = function (position) {
 
-  floObject.animation = function (name) {
-    if (floObject.animations[name] === undefined) {
-      floObject.animations[name] = {};
-    }
-
-    var animation = {
-      duration: function (name) {
-        return function (time) {
-          floObject.animations[name].duration = time;
-          return animation;
-        };
-      }(name),
-      nextPosition: function (name) {
-        return function (obj) {
-          floObject.animations[name].nextPosition = {};
-          for (key in obj) {
-            floObject.animations[name].nextPosition[key] = obj[key];
-          }
-          return animation;
-        };
-      }(name),
-      previousPosition: function (name) {
-        return function (obj) {
-          floObject.animations[name].previousPosition = obj;
-          return animation;
-        };
-      }(name),
-      ease: function (name) {
-        return function (easing) {
-          floObject.animations[name].ease = easing;
-          return animation;
-        };
-      }(name),
-      onComplete: function (name) {
-        return function (func) {
-          floObject.animations[name].onComplete = func;
-          return animation;
-        };
-      }(name),
-      invert: function (name) {
-        return function () {
-          var currentAnimation = floObject.animations[name].nextPosition;
-          for (key in currentAnimation) {
-            currentAnimation[key] *= -1;
-          }
-          return animation;
-        };
-      }(name),
-      reverse: function (name) {
-        return animation;
+    var floObject = {
+      position: position,
+      animations: {},
+      requestAnimationFrame: function (func) {
+        var requestAnimationFrame = window.requestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.msRequestAnimationFrame;
+        requestAnimationFrame(func);
       },
-      clear: function (name) {
-        return function (attr) {
-          if (attr !== undefined) {
-            floObject.animations[name][attr] = undefined;
-          } else {
-            floObject.animations[name] = undefined;
+      isFullscreen: function () {
+        return document.fullscreenElement ||
+          document.msFullscreenElement ||
+          document.mozFullScreenElement ||
+          document.webkitFullscreenElement;
+      },
+      enterFullscreen: function (element) {
+        if (element.requestFullscreen) {
+          element.requestFullscreen();
+        } else if (element.msRequestFullscreen) {
+          element.msRequestFullscreen();
+        } else if (element.mozRequestFullScreen) {
+          element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) {
+          element.webkitRequestFullscreen();
+        }
+      },
+      exitFullscreen: function () {
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+        } else if (document.msExitFullscreen) {
+          document.msExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+          document.webkitExitFullscreen();
+        }
+      },
+      onDocumentReady: function (func) {
+        var domContentLoaded = false;
+        var runFunction = function () {
+          if (!domContentLoaded) {
+            domContentLoaded = true;
+            func();
           }
-          return animation;
         };
-      }(name),
-      do: function (name) {
-        return function () {
-          floObject.do(name);
-        };
-        return animation;
+        if (document.addEventListener) {
+          document.addEventListener('DOMContentLoaded', runFunction, false);
+        }
+        if (document.attachEvent) {
+          document.attachEvent('onreadystatechange', runFunction);
+        }
+        document.onload = runFunction;
       }
     };
 
-    return animation;
-  };
+    floObject.animation = function (name) {
+      if (floObject.animations[name] === undefined) {
+        floObject.animations[name] = {};
+      }
 
-  floObject.do = function (name) {
-    if (name === undefined) {
-      floObject.currentAnimation === undefined;
-    } else {
-      floObject.startTime = new Date().getTime();
-      floObject.currentAnimation = name;
-      floObject.duration = floObject.animations[floObject.currentAnimation].duration;
+      var animation = {
+        duration: (function (name) {
+          return function (time) {
+            floObject.animations[name].duration = time;
+            return animation;
+          };
+        })(name),
+        nextPosition: (function (name) {
+          return function (obj) {
+            floObject.animations[name].nextPosition = {};
+            for (var key in obj) {
+              floObject.animations[name].nextPosition[key] = obj[key];
+            }
+            return animation;
+          };
+        })(name),
+        previousPosition: (function (name) {
+          return function (obj) {
+            floObject.animations[name].previousPosition = obj;
+            return animation;
+          };
+        })(name),
+        ease: (function (name) {
+          return function (easing) {
+            floObject.animations[name].ease = easing;
+            return animation;
+          };
+        })(name),
+        onComplete: (function (name) {
+          return function (func) {
+            floObject.animations[name].onComplete = func;
+            return animation;
+          };
+        })(name),
+        invert: (function (name) {
+          return function () {
+            var currentAnimation = floObject.animations[name].nextPosition;
+            for (var key in currentAnimation) {
+              currentAnimation[key] *= -1;
+            }
+            return animation;
+          };
+        })(name),
+        reverse: function () {
+          return animation;
+        },
+        clear: (function (name) {
+          return function (attr) {
+            if (attr !== undefined) {
+              floObject.animations[name][attr] = undefined;
+            } else {
+              floObject.animations[name] = undefined;
+            }
+            return animation;
+          };
+        })(name),
+        do: function (name) {
+          return function () {
+            floObject.do(name);
+            return animation;
+          };
+        }
+      };
 
-      floObject.previousPosition = function () {
-        var newPosition = {};
-        var animation = floObject.animations[name];
-        if (animation.previousPosition !== undefined) {
+      return animation;
+    };
+
+    floObject.do = function (name) {
+      if (name === undefined) {
+        floObject.currentAnimation = undefined;
+      } else {
+        floObject.startTime = new Date().getTime();
+        floObject.currentAnimation = name;
+        floObject.duration = floObject.animations[floObject.currentAnimation].duration;
+
+        floObject.previousPosition = (function () {
+          var key;
+          var newPosition = {};
+          var animation = floObject.animations[name];
+          if (animation.previousPosition !== undefined) {
+            for (key in floObject.position) {
+              newPosition[key] = animation.previousPosition[key] !== undefined ? animation.previousPosition[key] : floObject.position[key];
+            }
+            return newPosition;
+          }
           for (key in floObject.position) {
-            newPosition[key] = animation.previousPosition[key] !== undefined ? animation.previousPosition[key] : floObject.position[key];
+            newPosition[key] = floObject.position[key];
           }
           return newPosition;
+        })();
+      }
+    };
+
+    floObject.animate = function () {
+      var multiplier = Math.min((new Date().getTime() - floObject.startTime) / floObject.duration, 1);
+
+      if (floObject.currentAnimation) {
+        var animation = floObject.animations[floObject.currentAnimation];
+
+        for (var key in animation.nextPosition) {
+          floObject.position[key] = animation.nextPosition[key] * multiplier + floObject.previousPosition[key] * (1 - multiplier);
         }
-        for (key in floObject.position) {
-          newPosition[key] = floObject.position[key];
+
+        if (multiplier === 1) {
+          floObject.do(animation.onComplete);
         }
-        return newPosition;
-      }();
-    }
+      }
+    };
+
+    return floObject;
   };
 
-  floObject.animate = function () {
-    var multiplier = Math.min((new Date().getTime() - floObject.startTime) / floObject.duration, 1);
+  window.flo = flo;
 
-    if (floObject.currentAnimation) {
-      var animation = floObject.animations[floObject.currentAnimation];
-
-      for (key in animation.nextPosition) {
-        floObject.position[key] = animation.nextPosition[key] * multiplier + floObject.previousPosition[key] * (1 - multiplier);
-      }
-
-      if (multiplier === 1) {
-        floObject.do(animation.onComplete);
-      }
-    }
-  };
-
-  return floObject;
-
-};
+})();
